f37d2a1b6e7a5a58e9955c82e24c20af0766b4a6
Anusha Muthekepalli <anusharm@uic.edu>
2023-11-12 19:34:38 -0600

    yayyy 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { GithubLogo } from '@phosphor-icons/react'
 import './App.css'
+import { LinkedinLogo } from '@phosphor-icons/react/dist/ssr'
 
 function App() {
 
 
   return (
     <div>
+      <img src="anusha.jpg" width="400px" height="100%" />
       <h1>Anusha Muthekepalli</h1>
-      <h2>made by Anusha Muthekepalli</h2>
+      <a className='github' target="_blank" href="https://github.com/anush18"><GithubLogo size={32} weight="bold" /></a>
+      <a target="_blank" href="https://www.linkedin.com/in/anusha-muthekepalli"><LinkedinLogo size={32} weight="bold" /></a>
     </div>
   )
 }
