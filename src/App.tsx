6c47336c8a1f761da99c0d5662ac2fa759719e30
Anusha Muthekepalli <anusharm@uic.edu>
2023-11-12 19:02:00 -0600

    yayyyy

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div>
+      <h1>Anusha Muthekepalli</h1>
+      <h2>made by Anusha Muthekepalli</h2>
+    </div>
+  )
+}
+
+export default App
